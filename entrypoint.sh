ac18e6d339d72a6fae7a5ae6ff9991c043f7dd31
the-old-git <118171071+the-old-git@users.noreply.github.com>
2023-10-17 13:35:31 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo "==================="
-
 git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${INPUT_EMAIL}"
 git config --global --add safe.directory /github/workspace
@@ -9,5 +8,4 @@ python3 /usr/bin/feed.py
 
 git add -A && git commit -m "Update Feed"
 git push --set-upstream origin main
-
 echo "==================="
